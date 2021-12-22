a27af3657a7be7b29f606ab7e5125cadbfcb8e17
KozyrevOleg <o.kozyrev@inbox.ru>
2021-12-22 22:36:07 +0300

    Create a gradle build + gradle wrapper

---
+++ Tests/src/main/java/ru/stqa/pft/sandbox/MBS.java
@@ -0,0 +1,12 @@
+public class MBS {
+
+public static void main(String[] args) {
+
+System.out.println ("MBS");
+  System.out.println ("MBS");
+  System.out.println ("MBS");
+  System.out.println ("MBS");
+  System.out.println ("MBS");
+
+  }
+}
